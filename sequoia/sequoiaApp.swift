45e83d126d56fdd0977d60e2e7c43aa34db3696d
chengmengmeng <chengmengmeng.chris77@bytedance.com>
2023-04-26 21:40:00 +0800

    Initial Commit

---
+++ sequoia/sequoiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  sequoiaApp.swift
+//  sequoia
+//
+//  Created by chengmengmeng on 2023/4/26.
+//
+
+import SwiftUI
+
+@main
+struct sequoiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
